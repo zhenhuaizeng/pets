e4f4c1f94a7bdd3f6f67128ac203bc2e7afb92e3
Jack Zeng <zeng.zhenhuai@student.greenriver.edu>
2023-01-12 17:25:04 -0500

    Add Home Page

--- index.php
+++ index.php
@@ -16,7 +16,9 @@ $f3 = Base::instance();
 //Define a default route ("home page for hello project")
 
 $f3 -> route('GET /',function(){
-    echo '<h1>Pet Home</h1>';
+    //echo '<h1>Pet Home</h1>';
+    $view = new Template();
+    echo $view -> render('views/home.html');
 });
 
 $f3 -> run();
